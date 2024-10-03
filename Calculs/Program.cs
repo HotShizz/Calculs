dd5a6e2ddf4faadb13c807adfd54299253215450
Shane Hartford <sthartford@gmail.com>
2024-10-03 12:12:52 +0200

    optimisation de code avec utilisation du switch avec carecteres

--- Calculs/Program.cs
+++ Calculs/Program.cs
@@ -7,35 +7,27 @@ namespace Calculs
     /// </summary>
     class Program
     {
-        private static int GetUserResponse(bool gameChoice)
+        private static int GetUserResponse()
         {
-            bool isInt = false;
             int response = 0;
-            while (!isInt)
+            do
             {
                 try
                 {
                     response = int.Parse(Console.ReadLine());
-                    isInt = true;
+                    return response;
                 }
                 catch
                 {
-                    if (gameChoice)
-                    {
-                        Console.WriteLine("Erreur de Saisie. Merci de choissir un element dans la liste : ");
-                        GetChoice();
-                    }
-                    else
-                    {
-                        Console.WriteLine("Erreur de Saisie. Merci de saisir un nombre Entier");
-                    }
+                    Console.WriteLine();
+                    Console.WriteLine("Erreur de Saisie. Merci de saisir un nombre Entier : ");
                 }
-            }
-            return response;
+            } while (true);
         }
 
         private static int[] DoMath(int GameType)
         {
+            Console.Clear();
             Random rand = new Random(); // outil de génération de nombre aléatoire
             int[] results = new int[3]; // mémorisation de nombres aléatoires
             results[0] = rand.Next(1, 10);
@@ -51,60 +43,63 @@ namespace Calculs
             }
             return results;
         }
-        private static int GetChoice()
+        private static char GetChoice()
         {
-            Console.WriteLine("Addition ....................... 1");
-            Console.WriteLine("Multiplication ................. 2");
-            Console.WriteLine("Quitter ........................ 0");
-            Console.Write("Choix :                          ");
-            return 0;
+            char usersSelection;
+            do
+            {
+                Console.WriteLine("Addition ....................... 1");
+                Console.WriteLine("Multiplication ................. 2");
+                Console.WriteLine("Quitter ........................ 0");
+                Console.Write("Choix :                          ");
+                usersSelection = char.ToUpper(Console.ReadKey().KeyChar);
+                switch (usersSelection)
+                {
+                    case '0':
+                    case '1':
+                    case '2':
+                        return usersSelection;
+                    default:
+                        Console.WriteLine();
+                        Console.WriteLine("Erreur de saisie. Merci de choissir un élément dans la liste.");
+                        Console.WriteLine();
+                        break;
+                }
+            } while (true);
         }
         static void Main(string[] args)
         {
             
             // variables
-            int[] solution= new int[3]; // calcul de la solution
-            int reponse; // saisie de la réponse de l'utilisateur
-            int choix=4; // saisie du choix de l'utilsiateur
-            int counter = 0;
+            int[] solution= new int[3];
+            int reponse;
+            char choix='4';
             // boucle sur le menu
-            while (choix != 0)
+            while (choix != '0')
             {
-                choix = 4;// affiche le menu et saisi le choix
-                while (choix > 2 || choix < 0)
-                {
-                    if (counter > 0)
-                    {
-                        Console.WriteLine("Erreur de Saisie. Merci de choissir un element dans la liste : ");
-                    }
-                    GetChoice();
-                    choix = GetUserResponse(true);
-                    counter++;
-                }
-                counter = 0;
                 // traitement des choix
-                if (choix == 1)
+                if (choix == '1')
                 {
                     solution = DoMath(1);
                     // saisie de la réponse
-                    Console.Write(solution[0] +" + " + solution[1] + " = ");
-                        reponse = GetUserResponse(false);
-                        // comparaison avec la bonne réponse
-                        if (reponse == solution [2])
-                        {
-                            Console.WriteLine("Bravo !");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Faux : " + solution[0] + " + " + solution[1] + " = " + solution[2]);
-                        }
-                    }
-                    else if (choix==2)
+                    Console.Write(solution[0] + " + " + solution[1] + " = ");
+                    reponse = GetUserResponse();
+                    // comparaison avec la bonne réponse
+                    if (reponse == solution[2])
                     {
+                        Console.WriteLine("Bravo !");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Faux : " + solution[0] + " + " + solution[1] + " = " + solution[2]);
+                    }
+                }
+                else if (choix == '2')
+                {
                     solution = DoMath(2);
                     // saisie de la réponse
                     Console.Write(solution[0] + " X " + solution[1] + " = ");
-                    reponse = GetUserResponse(false);
+                    reponse = GetUserResponse();
                     // comparaison avec la bonne réponse
                     if (reponse == solution[2])
                     {
@@ -114,8 +109,8 @@ namespace Calculs
                     {
                         Console.WriteLine("Faux : " + solution[0] + " X " + solution[1] + " = " + solution[2]);
                     }
-                    choix = 4;
-               }
+                }
+                choix = GetChoice();
             }
         }
     }
